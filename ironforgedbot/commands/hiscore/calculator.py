fa155ab8429fb52359e97d207e17c14032133501
barnz <hi@barnz.dev>
2024-04-27 19:32:00 +0100

    expose activity kc

--- ironforgedbot/commands/hiscore/calculator.py
+++ ironforgedbot/commands/hiscore/calculator.py
@@ -21,22 +21,27 @@ class SkillInfo(TypedDict):
     points: int
 
 
+class ActivityInfo(TypedDict):
+    kc: int
+    points: int
+
+
 def score_total(player_name: str):
     data = _fetch_data(player_name)
     skills_score = _get_skills_info(data)
-    activities_score = _get_activities_score(data)
+    activities_score = _get_activities_info(data)
     return skills_score, activities_score
 
 
 def points_total(player_name: str) -> int:
-    skills, activities_score = score_total(player_name)
+    skills, activities = score_total(player_name)
     points = 0
 
     for _, skill in skills.items():
         points += skill["points"]
 
-    for _, v in activities_score.items():
-        points += v
+    for _, activity in activities.items():
+        points += activity["points"]
 
     return points
 
@@ -55,7 +60,7 @@ def _fetch_data(player_name: str):
 
 
 def _get_skills_info(score_data) -> Dict[str, SkillInfo]:
-    skill_points = {}
+    skills = {}
 
     for skill in score_data["skills"]:
         if not SKILLS.has_value(skill["name"]):
@@ -87,17 +92,17 @@ def _get_skills_info(score_data) -> Dict[str, SkillInfo]:
         if 0 == points:
             continue
 
-        skill_points[skill_constant] = {
+        skills[skill_constant] = {
             "xp": experience,
             "level": skill_level,
             "points": points,
         }
 
-    return skill_points
+    return skills
 
 
-def _get_activities_score(score_data) -> Dict[str, int]:
-    activity_points = {}
+def _get_activities_info(score_data) -> Dict[str, ActivityInfo]:
+    activities = {}
 
     for activity in score_data["activities"]:
         if not ACTIVITIES.has_value(activity["name"]):
@@ -115,6 +120,6 @@ def _get_activities_score(score_data) -> Dict[str, int]:
         if 0 == points:
             continue
 
-        activity_points[activity_constant] = points
+        activities[activity_constant] = {"kc": kc, "points": points}
 
-    return activity_points
+    return activities
