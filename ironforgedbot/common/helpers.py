28fee3d06897d074191a6f61d7989e12dfdc12df
barnz <hi@barnz.dev>
2024-05-22 20:34:00 +0100

    create method to validate whether member has a role or not

--- ironforgedbot/common/helpers.py
+++ ironforgedbot/common/helpers.py
@@ -49,15 +49,24 @@ def validate_protected_request(
     interaction: Interaction, playername: str, required_role: str
 ) -> Tuple[Member, str]:
     member, playername = validate_user_request(interaction, playername)
+    has_role = validate_member_has_role(member, required_role)
+
+    if not has_role:
+        raise ValueError(
+            f"Member '{member.display_name}' does not have permission for this action"
+        )
+
+    return member, playername
+
+
+def validate_member_has_role(member: Member, required_role: str) -> bool:
     roles = member.roles
 
     for role in roles:
         if role.name == required_role:
-            return member, playername
+            return True
 
-    raise ValueError(
-        f"Member '{member.display_name}' does not have permission for this action"
-    )
+    return False
 
 
 def find_member_by_nickname(guild: Guild, target_name: str):
