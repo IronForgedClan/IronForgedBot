0ad3ebf74493ee35b767a3d2e74fc8782f38d4c5
barnz <hi@barnz.dev>
2024-07-30 20:07:20 +0100

    add preload of hard requirements to main

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 import ironforgedbot.logging_config  # pyright: ignore  # noqa: F401 # isort:skip
-
 import argparse
 import logging
+import os
+import sys
+
 import discord
 
 from ironforgedbot.client import DiscordClient
@@ -10,6 +12,7 @@ from ironforgedbot.config import CONFIG
 from ironforgedbot.storage.data import BOSSES, CLUES, RAIDS, SKILLS
 from ironforgedbot.storage.sheets import SheetsStorage
 from ironforgedbot.storage.types import IngotsStorage
+from ironforgedbot.storage.sheets import STORAGE
 
 logger = logging.getLogger(__name__)
 
@@ -22,6 +25,16 @@ if __name__ == "__main__":
         help="Uploads commands to discord server.",
     )
     args = parser.parse_args()
+
+    # Preload hard requirements
+    if CONFIG and STORAGE and BOSSES and CLUES and RAIDS and SKILLS:
+        pass
+
+    try:
+        os.makedirs(CONFIG.TEMP_DIR, exist_ok=True)
+    except PermissionError:
+        logging.critical(f"Unable to create temp directory: {CONFIG.TEMP_DIR}")
+        sys.exit(1)
 
     # TODO: We lock the bot down with oauth perms; can we shrink intents to match?
     intents = discord.Intents.default()
