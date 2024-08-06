421851a6e028bc36708494153b4fd95a355a62f3
barnz <hi@barnz.dev>
2024-08-06 18:49:04 +0100

    update tests

--- tests/data_test.py
+++ tests/data_test.py
@@ -1,3 +1,4 @@
+import json
 import unittest
 from typing import NotRequired, TypedDict
 from unittest.mock import mock_open, patch
@@ -15,49 +16,48 @@ _test_file = "test.json"
 
 
 class TestDataLoader(unittest.TestCase):
-    @patch("ironforgedbot.storage.data.logger.error")
+    @patch(
+        "builtins.open",
+        new_callable=mock_open,
+        read_data='[{"name": "hitpoints", "display_name": "hp", "order": 1}]',
+    )
+    def test_load_json_data(self, mock_file):
+        result = load_json_data(_test_file, TestSkill)
+        expected = [{"name": "hitpoints", "display_name": "hp", "order": 1}]
+        self.assertEqual(expected, result)
+        mock_file.assert_called_once_with(_test_file, "r")
+
     @patch(
         "builtins.open",
         new_callable=mock_open,
         read_data="hello world",
     )
-    def test_load_json_data_fail_invalid_json(self, mock_file, mock_log):
-        result = load_json_data(_test_file, TestSkill)
-        self.assertIsNone(result)
-        mock_file.assert_called_once_with(_test_file, "r")
-        mock_log.assert_called_once_with("Error decoding json")
+    def test_load_json_data_fail_invalid_json(self, mock_file):
+        with self.assertRaises(json.decoder.JSONDecodeError):
+            load_json_data(_test_file, TestSkill)
+            mock_file.assert_called_once_with(_test_file, "r")
 
-    @patch("ironforgedbot.storage.data.logger.error")
     @patch(
         "builtins.open",
         new_callable=mock_open,
         read_data='{"name": "hitpoints", "order": 1}',
     )
-    def test_load_json_data_fail_no_list(self, mock_file, mock_log):
-        result = load_json_data(_test_file, TestSkill)
-        self.assertIsNone(result)
-        mock_file.assert_called_once_with(_test_file, "r")
-        mock_log.assert_called_once_with(
-            f"{_test_file}: does not contain an array/list"
-        )
+    def test_load_json_data_fail_no_list(self, mock_file):
+        with self.assertRaises(TypeError):
+            load_json_data(_test_file, TestSkill)
+            mock_file.assert_called_once_with(_test_file, "r")
 
-    @patch("ironforgedbot.storage.data.logger.error")
     @patch("builtins.open", new_callable=mock_open, read_data='["hitpoints"]')
-    def test_load_json_data_fail_no_object(self, mock_file, mock_log):
-        result = load_json_data(_test_file, TestSkill)
-        self.assertIsNone(result)
-        mock_file.assert_called_once_with(_test_file, "r")
-        mock_log.assert_called_once_with(f"{_test_file}: does not contain object/dict")
+    def test_load_json_data_fail_no_object(self, mock_file):
+        with self.assertRaises(TypeError):
+            load_json_data(_test_file, TestSkill)
+            mock_file.assert_called_once_with(_test_file, "r")
 
-    @patch("ironforgedbot.storage.data.logger.error")
     @patch("builtins.open", new_callable=mock_open, read_data='[{"name": "hitpoints"}]')
-    def test_load_json_data_fail_missing_key(self, mock_file, mock_log):
-        result = load_json_data(_test_file, TestSkill)
-        self.assertIsNone(result)
-        mock_file.assert_called_once_with(_test_file, "r")
-        mock_log.assert_called_once_with(
-            f"{_test_file}: object missing key (order) for type (TestSkill)"
-        )
+    def test_load_json_data_fail_missing_key(self, mock_file):
+        with self.assertRaises(KeyError):
+            load_json_data(_test_file, TestSkill)
+            mock_file.assert_called_once_with(_test_file, "r")
 
     @patch(
         "builtins.open",
@@ -67,16 +67,5 @@ class TestDataLoader(unittest.TestCase):
     def test_load_json_data_populate_optional_fields_if_empty(self, mock_file):
         result = load_json_data(_test_file, TestSkill)
         expected = [{"name": "hitpoints", "display_name": None, "order": 1}]
-        self.assertEqual(expected, result)
-        mock_file.assert_called_once_with(_test_file, "r")
-
-    @patch(
-        "builtins.open",
-        new_callable=mock_open,
-        read_data='[{"name": "hitpoints", "display_name": "hp", "order": 1}]',
-    )
-    def test_load_json_data(self, mock_file):
-        result = load_json_data(_test_file, TestSkill)
-        expected = [{"name": "hitpoints", "display_name": "hp", "order": 1}]
         self.assertEqual(expected, result)
         mock_file.assert_called_once_with(_test_file, "r")
