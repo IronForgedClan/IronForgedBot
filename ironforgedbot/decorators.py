c9acf958264063e0c89f6f41b60e9b2f4c562d07
barnz <hi@barnz.dev>
2024-08-08 21:45:19 +0100

    add require_role decorator tests

--- ironforgedbot/decorators.py
+++ ironforgedbot/decorators.py
@@ -12,21 +12,25 @@ def require_role(role_name: str):
     def decorator(func):
         @functools.wraps(func)
         async def wrapper(*args, **kwargs):
-            if role_name is None:
+            if role_name is None or len(role_name) < 1:
                 raise ValueError(f"No role provided to decorator ({func.__name__})")
 
             interaction = args[0]
             if not isinstance(interaction, discord.Interaction):
                 raise ReferenceError(
-                    f"Expected discord.Interaction as a first argument for '{func.__name__}'"
+                    f"Expected discord.Interaction as first argument ({func.__name__})"
                 )
 
             if not interaction.guild:
-                raise ValueError("Unable to access guild information")
+                raise ValueError(
+                    f"Unable to access guild information ({func.__name__})"
+                )
 
             member = interaction.guild.get_member(interaction.user.id)
             if not member:
-                raise ValueError("Unable to access caller's guild membership")
+                raise ValueError(
+                    f"Unable to verify caller's guild membership ({func.__name__})"
+                )
 
             has_role = validate_member_has_role(member, role_name)
             if not has_role:
