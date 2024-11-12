06e3e6ca072b2dbd2392bbbc6d04210c31a29dd5
barnz <hi@barnz.dev>
2024-11-12 00:28:51 +0000

    add total count and cleanup responses

--- ironforgedbot/commands/admin/cmd_get_role_members.py
+++ ironforgedbot/commands/admin/cmd_get_role_members.py
@@ -16,20 +16,21 @@ async def cmd_get_role_members(
 ):
     assert interaction.guild
     role = normalize_discord_string(role)
+    count = 0
     output = ""
 
     for member in interaction.guild.members:
         if validate_member_has_role(member, role):
+            count += 1
             output += f"{normalize_discord_string(member.display_name)}, "
 
     if len(output) < 1:
         return await interaction.followup.send(
-            f"No members found with the role **{role}**."
+            f"No members with role '**{role}**' found."
         )
 
-    output = output[:-2]
     discord_file = discord.File(
-        fp=io.BytesIO(output.encode("utf-8")),
+        fp=io.BytesIO(output[:-2].encode("utf-8")),
         filename=(
             f"{role.lower().replace(' ', '_')}_role_list_"
             f"{datetime.now().strftime("%Y%m%d_%H%M%S")}.txt"
@@ -39,7 +40,7 @@ async def cmd_get_role_members(
     return await interaction.followup.send(
         (
             "## Member Role List\n"
-            f"A complete list of all members currently in this server with the role: **{role}**."
+            f"Found **{count}** members with the role '**{role}**'."
         ),
         file=discord_file,
     )
