859ba64748d3642f79f54c8353f0e3696641f797
barnz <hi@barnz.dev>
2025-01-15 19:57:20 +0000

    improve prospect effect error handling and reporting

--- ironforgedbot/effects/add_prospect_role.py
+++ ironforgedbot/effects/add_prospect_role.py
@@ -3,14 +3,20 @@ import logging
 import discord
 
 from ironforgedbot.common.helpers import normalize_discord_string
+from ironforgedbot.common.roles import ROLE
 from ironforgedbot.storage.sheets import STORAGE
 from ironforgedbot.storage.types import StorageError
 
 logger = logging.getLogger(__name__)
 
 
-async def add_prospect_role(member: discord.Member):
-    logger.info(f"{member.display_name} has been made a prospect")
+async def add_prospect_role(
+    report_channel: discord.TextChannel, member: discord.Member
+):
+    await report_channel.send(
+        f":information: {member.mention} has been given the "
+        f"{ROLE.PROSPECT} role, saving timestamp."
+    )
 
     try:
         storage_member = await STORAGE.read_member(
@@ -18,9 +24,13 @@ async def add_prospect_role(member: discord.Member):
         )
     except StorageError as e:
         logger.error(e)
+        await report_channel.send("An error occured. Please contact the Discord Team.")
         return
 
-    assert storage_member
+    if not storage_member:
+        await report_channel.send("An error occured. Please contact the Discord Team.")
+        return
+
     storage_member.joined_date = datetime.now(timezone.utc).isoformat()
 
     return await STORAGE.update_members([storage_member], "BOT", "Added Prospect role")
