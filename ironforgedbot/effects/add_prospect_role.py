72a5e719eb1b2ea051942a73bc80f3cbce1e149b
barnz <hi@barnz.dev>
2025-01-29 00:00:18 +0000

    if adding prospect role to user without member role attempt to add correct roles

--- ironforgedbot/effects/add_prospect_role.py
+++ ironforgedbot/effects/add_prospect_role.py
@@ -1,9 +1,11 @@
-from datetime import datetime, timezone
 import logging
+from datetime import datetime, timezone
+
 import discord
 
 from ironforgedbot.common.helpers import (
     datetime_to_discord_relative,
+    get_discord_role,
     normalize_discord_string,
 )
 from ironforgedbot.common.roles import ROLE
@@ -17,11 +19,6 @@ logger = logging.getLogger(__name__)
 async def add_prospect_role(
     report_channel: discord.TextChannel, member: discord.Member
 ):
-    await report_channel.send(
-        f":information: {member.mention} has been given the "
-        f"{text_bold(ROLE.PROSPECT)} role, saving timestamp..."
-    )
-
     storage_member = None
     error = False
     try:
@@ -32,15 +29,26 @@ async def add_prospect_role(
         error = True
 
     if error or not storage_member:
-        return await report_channel.send(
-            f":warning: {text_bold('WARNING')}\nAdded the "
-            f"{text_bold(ROLE.PROSPECT)} role to a member that doesn't exist in "
-            f"storage. Timestamp can therefore not be saved.\n\n"
-            f"Please make sure {member.mention} has the "
-            f"{text_bold(ROLE.MEMBER)} role and has been successfully synchonized. "
-            f"Then add the {text_bold(ROLE.PROSPECT)} role again to successfully "
-            "save a timestamp. Adding both roles at once is only supported on mobile."
+        await report_channel.send(
+            f":information: {member.mention} has been given the "
+            f"{text_bold(ROLE.PROSPECT)} role without having the {text_bold(ROLE.MEMBER)} "
+            "role. Adding correct roles to member and trying again..."
         )
+
+        member_role = get_discord_role(report_channel.guild, ROLE.MEMBER)
+        prospect_role = get_discord_role(report_channel.guild, ROLE.PROSPECT)
+        if not member_role or not prospect_role:
+            raise ValueError("Unable to access member or prospect role")
+
+        await member.add_roles(member_role)
+        await member.remove_roles(prospect_role)
+        await member.add_roles(prospect_role)
+        return
+
+    await report_channel.send(
+        f":information: {member.mention} has been given the "
+        f"{text_bold(ROLE.PROSPECT)} role, saving timestamp..."
+    )
 
     now = datetime.now(timezone.utc)
     storage_member.joined_date = now.isoformat()
