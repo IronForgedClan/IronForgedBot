411e7fdf6365333bba9aceabe47a1ca69e82df60
barnz <hi@barnz.dev>
2025-01-29 19:24:02 +0000

    ignore name change events for non-members

--- ironforgedbot/effects/nickname_change.py
+++ ironforgedbot/effects/nickname_change.py
@@ -1,6 +1,7 @@
 import logging
 import discord
 
+from ironforgedbot.common.roles import ROLE, check_member_has_role
 from ironforgedbot.common.text_formatters import text_bold
 from ironforgedbot.tasks.job_sync_members import job_sync_members
 
@@ -10,6 +11,9 @@ logger = logging.getLogger(__name__)
 async def nickname_change(
     report_channel: discord.TextChannel, before: discord.Member, after: discord.Member
 ):
+    if not check_member_has_role(after, ROLE.MEMBER):
+        return
+
     await report_channel.send(
         f":information: Name change detected: {text_bold(before.display_name)} → "
         f"{text_bold(after.display_name)}. Initiating member sync..."
