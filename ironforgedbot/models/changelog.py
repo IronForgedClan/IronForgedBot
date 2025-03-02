ab35229bc2b3272f8b71047625bd8e1542f47da4
barnz <hi@barnz.dev>
2025-03-02 15:25:15 +0000

    wip models

---
+++ ironforgedbot/models/changelog.py
@@ -0,0 +1,27 @@
+from datetime import datetime
+from enum import Enum
+from sqlalchemy import DateTime, ForeignKey, Integer, String
+from sqlalchemy.orm import Mapped, mapped_column
+
+from ironforgedbot.database.database import Base
+
+
+class ChangeType(Enum):
+    NAME_CHANGE = 1
+    ACTIVE_CHANGE = 2
+    ADD_INGOTS = 3
+    REMOVE_INGOTS = 4
+    RANK_CHANGE = 5
+
+
+class Changelog(Base):
+    __tablename__ = "changelog"
+
+    id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    member_id: Mapped[str] = mapped_column(ForeignKey("members.id"), nullable=False)
+    admin_id: Mapped[str] = mapped_column(ForeignKey("members.id"), nullable=False)
+    change_type: Mapped[ChangeType] = mapped_column(Integer)
+    previous_value: Mapped[str] = mapped_column(String)
+    new_value: Mapped[str] = mapped_column(String)
+    note: Mapped[str] = mapped_column(String)
+    timestamp: Mapped[datetime] = mapped_column(DateTime)
---
+++ ironforgedbot/models/member.py
@@ -0,0 +1,17 @@
+from datetime import datetime
+from sqlalchemy import BigInteger, Boolean, DateTime, Integer, String, Uuid
+from sqlalchemy.orm import Mapped, mapped_column
+from ironforgedbot.common.ranks import RANK
+from ironforgedbot.database.database import Base
+
+
+class Member(Base):
+    __tablename__ = "members"
+
+    id: Mapped[str] = mapped_column(Uuid, primary_key=True)
+    discord_id: Mapped[int] = mapped_column(BigInteger, unique=True, nullable=False)
+    active: Mapped[bool] = mapped_column(Boolean, default=True)
+    nickname: Mapped[str] = mapped_column(String, unique=True, nullable=False)
+    ingots: Mapped[int] = mapped_column(Integer, default=0)
+    rank: Mapped[RANK] = mapped_column(String)
+    joined_date: Mapped[datetime] = mapped_column(DateTime)
