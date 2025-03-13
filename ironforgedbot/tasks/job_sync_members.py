8fb6c53ebb54980064cd80da407b31994a2091a0
barnz <hi@barnz.dev>
2025-03-13 19:54:50 +0000

    cleaner processed time output

--- ironforgedbot/tasks/job_sync_members.py
+++ ironforgedbot/tasks/job_sync_members.py
@@ -7,7 +7,7 @@ import discord
 from tabulate import tabulate
 
 from ironforgedbot.commands.admin.sync_members import sync_members
-from ironforgedbot.common.helpers import datetime_to_discord_relative
+from ironforgedbot.common.helpers import datetime_to_discord_relative, format_duration
 from ironforgedbot.common.text_formatters import text_h2
 
 logger = logging.getLogger(__name__)
@@ -27,8 +27,14 @@ async def job_sync_members(
             "An unhandled error occurrend during member sync. Please check the logs."
         )
 
+    end_time = time.perf_counter()
+
     if len(members_change) < 1:
-        return await report_channel.send("**Member Sync**: No changes to report.")
+        return await report_channel.send(
+            "**Member Sync**: Processed in "
+            f"**{format_duration(start_time,end_time)}**. "
+            "No changes to report."
+        )
 
     output_table = tabulate(
         members_change, headers=["Member", "Action", "Reason"], tablefmt="simple"
@@ -39,12 +45,9 @@ async def job_sync_members(
         filename=f"sync_results_{datetime.now().strftime("%Y%m%d_%H%M%S")}.txt",
     )
 
-    end_time = time.perf_counter()
-    elapsed_time = end_time - start_time
-
     return await report_channel.send(
         f"{text_h2(" 🔁 Member Synchronization")}\n"
         f"Initiated at {datetime_to_discord_relative(now, 't')} and "
-        f"completed in **{elapsed_time:.4f}** seconds.",
+        f"completed in **{format_duration(start_time, end_time)}**.",
         file=discord_file,
     )
