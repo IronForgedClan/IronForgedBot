22a913263efda9d4f5823f5b8d010c44f23b6462
Barnz <hi@barnz.dev>
2025-08-08 14:06:11 +0100

    add missing import

--- ironforgedbot/models/score_history.py
+++ ironforgedbot/models/score_history.py
@@ -1,5 +1,5 @@
 from datetime import datetime, timezone
-from sqlalchemy import Integer, String
+from sqlalchemy import ForeignKey, Integer, String
 from sqlalchemy.orm import Mapped, mapped_column
 from ironforgedbot.database.database import Base
 from ironforgedbot.models.decorators import UTCDateTime
