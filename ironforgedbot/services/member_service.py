8cf45ebb0dfbdc6026301c7d553fd102767ad916
Barnz <hi@barnz.dev>
2025-09-06 16:35:02 +0100

    add tests for change nickname

--- ironforgedbot/services/member_service.py
+++ ironforgedbot/services/member_service.py
@@ -303,7 +303,7 @@ class MemberService:
         except Exception as e:
             logger.critical(e)
             await self.db.rollback()
-            raise
+            raise e
 
         return member
 
--- tests/services/member_service_test.py
+++ tests/services/member_service_test.py
@@ -563,17 +563,6 @@ class TestMemberService_CreateMember(unittest.IsolatedAsyncioTestCase):
             last_changed_date=now - timedelta(hours=1),
         )
 
-        expected_member = {
-            "id": old_member.id,
-            "discord_id": old_member.discord_id,
-            "active": False,
-            "nickname": old_member.nickname,
-            "ingots": old_member.ingots,
-            "rank": old_member.rank,
-            "joined_date": old_member.joined_date,
-            "last_changed_date": now,
-        }
-
         mock_db = AsyncMock()
         mock_db.add = MagicMock()
         mock_db.flush = AsyncMock()
@@ -587,5 +576,117 @@ class TestMemberService_CreateMember(unittest.IsolatedAsyncioTestCase):
                 _ = await service.disable_member(
                     old_member.id,
                 )
+
+        mock_db.rollback.assert_called()
+
+    @patch("ironforgedbot.services.member_service.datetime")
+    async def test_change_nickname_should_succeed(self, mock_datetime) -> None:
+        now = datetime(2025, 1, 1, 12, 0, 0)
+        mock_datetime.now.return_value = now
+
+        new_nickname = "zezima"
+
+        old_member = Member(
+            id="123",
+            discord_id="333",
+            active=True,
+            nickname="bob",
+            ingots=2277,
+            rank=RANK.IRON,
+            joined_date=now - timedelta(days=20),
+            last_changed_date=now - timedelta(hours=1),
+        )
+
+        expected_member = {
+            "id": old_member.id,
+            "discord_id": old_member.discord_id,
+            "active": old_member.active,
+            "nickname": new_nickname,
+            "ingots": old_member.ingots,
+            "rank": old_member.rank,
+            "joined_date": old_member.joined_date,
+            "last_changed_date": now,
+        }
+
+        mock_db = AsyncMock()
+        mock_db.add = MagicMock()
+        mock_db.flush = AsyncMock()
+        mock_db.commit = AsyncMock()
+        service = MemberService(db=mock_db)
+
+        with patch.object(MemberService, "get_member_by_id", return_value=old_member):
+            result: Member = await service.change_nickname(old_member.id, "zezima")
+
+        expected_changelog_activity = {
+            "member_id": old_member.id,
+            "admin_id": None,
+            "change_type": ChangeType.NAME_CHANGE,
+            "previous_value": "bob",
+            "new_value": new_nickname,
+            "comment": "Updated nickname",
+            "timestamp": now,
+        }
+
+        result_changelog_activity = mock_db.add.call_args_list[0][0][0]
+
+        self.assertEqual(mock_db.add.call_count, 1)
+
+        self.assertIsInstance(result_changelog_activity, Changelog)
+
+        for key, value in expected_changelog_activity.items():
+            actual_value = getattr(result_changelog_activity, key, None)
+            self.assertEqual(
+                actual_value, value, f"Expected {key}={value}, but got {actual_value}"
+            )
+
+        self.assertIsInstance(result, Member)
+        for key, value in expected_member.items():
+            actual_value = getattr(result, key, None)
+            self.assertEqual(
+                actual_value, value, f"Expected {key}={value}, but got {actual_value}"
+            )
+
+        mock_db.commit.assert_called()
+
+    async def test_change_nickname_should_raise_MemberNotFoundException(self) -> None:
+        mock_db = AsyncMock()
+        service = MemberService(db=mock_db)
+
+        with patch.object(MemberService, "get_member_by_id", return_value=None):
+            with self.assertRaises(MemberNotFoundException):
+                _ = await service.change_nickname(
+                    "123",
+                    "zezima",
+                )
+
+    @patch("ironforgedbot.services.member_service.datetime")
+    async def test_update_nickname_exception_should_rollback(
+        self, mock_datetime
+    ) -> None:
+        now = datetime(2025, 1, 1, 12, 0, 0)
+        mock_datetime.now.return_value = now
+
+        old_member = Member(
+            id="123",
+            discord_id="333",
+            active=True,
+            nickname="zezima",
+            ingots=2277,
+            rank=RANK.IRON,
+            joined_date=now - timedelta(days=20),
+            last_changed_date=now - timedelta(hours=1),
+        )
+
+        mock_db = AsyncMock()
+        mock_db.add = MagicMock()
+        mock_db.flush = AsyncMock()
+        mock_db.rollback = AsyncMock()
+        mock_db.commit = AsyncMock()
+        mock_db.commit.side_effect = Exception()
+        service = MemberService(db=mock_db)
+
+        with patch.object(MemberService, "get_member_by_id", return_value=old_member):
+            with self.assertRaises(Exception):
+                _ = await service.change_nickname(old_member.id, "bob")
 
         mock_db.rollback.assert_called()
