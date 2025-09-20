99ef628b2eebda612b84d218659cf3a54bdcddb1
Barnz <hi@barnz.dev>
2025-09-20 16:04:58 +0100

    clean up test helpers

--- tests/helpers.py
+++ tests/helpers.py
@@ -1,12 +1,9 @@
 import functools
 import random
-import sys
 from typing import Any, List, Optional
 from unittest.mock import AsyncMock, Mock
 import discord
-import wom
 from ironforgedbot.common.roles import ROLE
-from ironforgedbot.models.score import ScoreBreakdown, SkillScore, ActivityScore
 
 VALID_CONFIG = {
     "TEMP_DIR": "/tmp",
@@ -20,53 +17,6 @@ VALID_CONFIG = {
     "TRICK_OR_TREAT_CHANNEL_ID": "",
     "RAFFLE_CHANNEL_ID": "123456",
 }
-
-mock_score_breakdown = ScoreBreakdown(skills=[], clues=[], raids=[], bosses=[])
-
-"""
-mock_score_breakdown = ScoreBreakdown(
-    skills=[
-        SkillScore{
-            name="Slayer"
-            display_name="Slayer"
-            display_order= 1
-            emoji_key="Slayer"
-            level=67
-            xp=547953
-            points=18
-        }
-    ],
-    clues=[
-        {
-            "name": "Clue Scrolls (beginner)",
-            "display_name": "Beginner",
-            "display_order": 1,
-            "emoji_key": "ClueScrolls_Beginner",
-            "kc": 100,
-            "points": 10,
-        },
-    ],
-    raids=[
-        {
-            "name": "Tombs of Amascut",
-            "display_order": 4,
-            "emoji_key": "TombsOfAmascut",
-            "kc": 10,
-            "points": 10,
-        },
-    ],
-    bosses=[
-        {
-            "name": "Kraken",
-            "display_name": "Kraken",
-            "display_order": 1,
-            "emoji_key": "Kraken",
-            "kc": 70,
-            "points": 2,
-        }
-    ],
-)
-"""
 
 
 def create_mock_discord_interaction(
@@ -108,11 +58,8 @@ def create_mock_discord_guild(
     guild = Mock(spec=discord.Guild)
     guild.members = members or []
     guild.emojis = []
-    guild.roles = []
-    guild.member_count = len(members or [])
-    if roles:
-        for role in roles:
-            guild.roles.append(create_mock_discord_role(role))
+    guild.roles = [create_mock_discord_role(role) for role in (roles or [])]
+    guild.member_count = len(guild.members)
 
     return guild
 
@@ -127,11 +74,7 @@ def create_mock_discord_role(name: str) -> discord.Role:
 def create_test_member(
     name: str, roles: list[str], nick: Optional[str] = None
 ) -> discord.Member:
-    role_list = []
-    for role in roles:
-        mock_role = Mock(spec=discord.Role)
-        mock_role.name = role
-        role_list.append(mock_role)
+    role_list = [create_mock_discord_role(role) for role in roles]
 
     mock_member = Mock(spec=discord.Member)
     mock_member.bot = False
@@ -144,13 +87,6 @@ def create_test_member(
     mock_member.remove_roles = AsyncMock()
 
     return mock_member
-
-
-def create_mock_wom_client() -> wom.Client:
-    client = Mock(spec=wom.Client)
-    client.start = AsyncMock()
-
-    return client
 
 
 def mock_require_role(role_name: str, ephemeral: Optional[bool] = False):
@@ -175,41 +111,9 @@ def mock_require_channel(channel_ids: list):
     return decorator
 
 
-def mock_rate_limit(rate: int, limit: int):
-    def decorator(func):
-        @functools.wraps(func)
-        async def wrapper(*args, **kwargs):
-            return await func(*args, **kwargs)
-
-        return wrapper
-
-    return decorator
-
-
-def mock_singleton():
-    def decorator(func):
-        @functools.wraps(func)
-        async def wrapper(*args, **kwargs):
-            return await func(*args, **kwargs)
-
-        return wrapper
-
-    return decorator
-
-
 async def get_url_status_code(session, url, timeout=5):
     try:
         async with session.get(url, timeout=timeout) as response:
             return response.status
     except Exception as e:
         return str(e)
-
-
-def validate_embed(self, expected, actual):
-    self.assertEqual(actual.title, expected.title)
-    self.assertEqual(len(actual.fields), len(expected.fields))
-
-    for expected, actual in zip(expected.fields, actual.fields):
-        self.assertEqual(expected.name, actual.name)
-        self.assertEqual(expected.value, actual.value)
-        self.assertEqual(expected.inline, actual.inline)
