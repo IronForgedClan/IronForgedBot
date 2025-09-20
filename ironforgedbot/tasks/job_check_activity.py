fd8ba4a43eef8e44245cb214387997968d272156
Barnz <hi@barnz.dev>
2025-09-20 14:22:15 +0100

    fix bug in check activity implementation

--- ironforgedbot/tasks/job_check_activity.py
+++ ironforgedbot/tasks/job_check_activity.py
@@ -78,95 +78,100 @@ async def _find_inactive_users(
     wom_client = wom.Client(api_key=wom_api_key, user_agent="IronForged")
     await wom_client.start()
 
-    is_done = False
-    offset = 0
-    results = []
-    wom_grop_result = await wom_client.groups.get_details(wom_group_id)
+    try:
+        is_done = False
+        offset = 0
+        results = []
+        wom_grop_result = await wom_client.groups.get_details(wom_group_id)
 
-    if wom_grop_result.is_ok:
-        wom_group = wom_grop_result.unwrap()
-    else:
-        message = f"Got error, fetching WOM group: {wom_grop_result.unwrap_err()}"
-        logger.error(message)
-        await report_channel.send(content=message)
-        return None
-
-    while not is_done:
-        result = await wom_client.groups.get_gains(
-            wom_group_id,
-            metric=Metric.Overall,
-            period=Period.Month,
-            limit=DEFAULT_WOM_LIMIT,
-            offset=offset,
-        )
-        if result.is_ok:
-            offset += DEFAULT_WOM_LIMIT
-            details = result.unwrap()
-            for member_gains in details:
-                wom_member = _find_wom_member(wom_group, member_gains.player.id)
-
-                if wom_member is None:
-                    continue
-                if wom_member.player.username.lower() in absentees:
-                    continue
-
-                if wom_member.role == GroupRole.Dogsbody:
-                    continue
-
-                match wom_member.role:
-                    case GroupRole.Iron:
-                        xp_threshold = IRON_EXP_THRESHOLD
-                    case GroupRole.Mithril | GroupRole.Adamant:
-                        xp_threshold = MITHRIL_EXP_THRESHOLD
-                    case _:
-                        xp_threshold = RUNE_EXP_THRESHOLD
-
-                if member_gains.data.gained < xp_threshold:
-                    if not wom_member.role:
-                        role = "Unknown"
-                    elif wom_member.role == GroupRole.Helper:
-                        role = "Alt"
-                    elif wom_member.role == GroupRole.Collector:
-                        role = "Moderator"
-                    elif wom_member.role == GroupRole.Administrator:
-                        role = "Admin"
-                    elif wom_member.role in [
-                        GroupRole.Colonel,
-                        GroupRole.Brigadier,
-                        GroupRole.Admiral,
-                        GroupRole.Marshal,
-                    ]:
-                        role = "Staff"
-                    elif wom_member.role in [GroupRole.Deputy_owner, GroupRole.Owner]:
-                        role = "Owner"
-                    else:
-                        role = str(wom_member.role).title()
-
-                    days_since_progression = "unknown"
-                    if wom_member.player.last_changed_at:
-                        days_since_progression = render_relative_time(
-                            wom_member.player.last_changed_at
-                        )
-
-                    results.append(
-                        [
-                            member_gains.player.username,
-                            role,
-                            f"{int(member_gains.data.gained):,}",
-                            days_since_progression,
-                        ]
-                    )
-
-            if len(details) < DEFAULT_WOM_LIMIT:
-                is_done = True
+        if wom_grop_result.is_ok:
+            wom_group = wom_grop_result.unwrap()
         else:
-            message = f"Got error, fetching gains from WOM: {result.unwrap_err()}"
+            message = f"Got error, fetching WOM group: {wom_grop_result.unwrap_err()}"
             logger.error(message)
             await report_channel.send(content=message)
             return None
 
-    await wom_client.close()
-    return results
+        while not is_done:
+            result = await wom_client.groups.get_gains(
+                wom_group_id,
+                metric=Metric.Overall,
+                period=Period.Month,
+                limit=DEFAULT_WOM_LIMIT,
+                offset=offset,
+            )
+            if result.is_ok:
+                offset += DEFAULT_WOM_LIMIT
+                details = result.unwrap()
+                for member_gains in details:
+                    wom_member = _find_wom_member(wom_group, member_gains.player.id)
+
+                    if wom_member is None:
+                        continue
+                    if wom_member.player.username.lower() in absentees:
+                        continue
+
+                    if wom_member.role == GroupRole.Dogsbody:
+                        continue
+
+                    match wom_member.role:
+                        case GroupRole.Iron:
+                            xp_threshold = IRON_EXP_THRESHOLD
+                        case GroupRole.Mithril | GroupRole.Adamant:
+                            xp_threshold = MITHRIL_EXP_THRESHOLD
+                        case _:
+                            xp_threshold = RUNE_EXP_THRESHOLD
+
+                    if member_gains.data.gained < xp_threshold:
+                        if not wom_member.role:
+                            role = "Unknown"
+                        elif wom_member.role == GroupRole.Helper:
+                            role = "Alt"
+                        elif wom_member.role == GroupRole.Collector:
+                            role = "Moderator"
+                        elif wom_member.role == GroupRole.Administrator:
+                            role = "Admin"
+                        elif wom_member.role in [
+                            GroupRole.Colonel,
+                            GroupRole.Brigadier,
+                            GroupRole.Admiral,
+                            GroupRole.Marshal,
+                        ]:
+                            role = "Staff"
+                        elif wom_member.role in [
+                            GroupRole.Deputy_owner,
+                            GroupRole.Owner,
+                        ]:
+                            role = "Owner"
+                        else:
+                            role = str(wom_member.role).title()
+
+                        days_since_progression = "unknown"
+                        if wom_member.player.last_changed_at:
+                            days_since_progression = render_relative_time(
+                                wom_member.player.last_changed_at
+                            )
+
+                        results.append(
+                            [
+                                member_gains.player.username,
+                                role,
+                                f"{int(member_gains.data.gained):,}",
+                                days_since_progression,
+                            ]
+                        )
+
+                if len(details) < DEFAULT_WOM_LIMIT:
+                    is_done = True
+            else:
+                message = f"Got error, fetching gains from WOM: {result.unwrap_err()}"
+                logger.error(message)
+                await report_channel.send(content=message)
+                return None
+
+        return results
+    finally:
+        await wom_client.close()
 
 
 def _find_wom_member(group: GroupDetail, player_id: int) -> GroupMembership | None:
