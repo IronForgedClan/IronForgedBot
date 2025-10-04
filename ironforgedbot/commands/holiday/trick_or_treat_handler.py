8230ecc224d7e7f065e047c7b3a855dc21f4aabb
Barnz <hi@barnz.dev>
2025-10-04 19:14:13 +0100

    preserve comment formatting

--- ironforgedbot/commands/holiday/trick_or_treat_handler.py
+++ ironforgedbot/commands/holiday/trick_or_treat_handler.py
@@ -36,14 +36,16 @@ class TrickOrTreat(Enum):
     Total weight: 1000
     """
 
-    GIF = 343                      # 34.3% (1 in 2.9)
-    REMOVE_INGOTS_LOW = 143        # 14.3% (1 in 7.0)
-    ADD_INGOTS_LOW = 137           # 13.7% (1 in 7.3)
-    REMOVE_INGOTS_HIGH = 126       # 12.6% (1 in 7.9)
-    ADD_INGOTS_HIGH = 114          # 11.4% (1 in 8.8)
-    JOKE = 100                     # 10.0% (1 in 10.0)
-    REMOVE_ALL_INGOTS_TRICK = 34   # 3.4% (1 in 29.4)
-    JACKPOT_INGOTS = 3             # 0.3% (1 in 333.3)
+    # fmt: off
+    GIF = 343                      # 34.3% (1/2.9)
+    REMOVE_INGOTS_LOW = 143        # 14.3% (1/7.0)
+    ADD_INGOTS_LOW = 137           # 13.7% (1/7.3)
+    REMOVE_INGOTS_HIGH = 126       # 12.6% (1/7.9)
+    ADD_INGOTS_HIGH = 114          # 11.4% (1/8.8)
+    JOKE = 100                     # 10.0% (1/10.0)
+    REMOVE_ALL_INGOTS_TRICK = 34   # 3.4% (1/29.4)
+    JACKPOT_INGOTS = 3             # 0.3% (1/333.3)
+    # fmt: on
 
 
 class TrickOrTreatHandler:
