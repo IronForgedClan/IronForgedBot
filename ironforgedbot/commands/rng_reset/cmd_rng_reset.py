81a0e6dddc5887d3447cd4e102b6edff7ea2bad1
Barnz <hi@barnz.dev>
2025-10-21 19:06:35 +0100

    wip rng reset command

--- ironforgedbot/commands/rng_reset/cmd_rng_reset.py
+++ ironforgedbot/commands/rng_reset/cmd_rng_reset.py
@@ -1,8 +1,8 @@
+import asyncio
 import logging
 import random
 
 import discord
-from discord import app_commands
 
 from ironforgedbot.common.logging_utils import log_command_execution
 from ironforgedbot.common.responses import build_response_embed
@@ -21,25 +21,41 @@ async def cmd_rng_reset(interaction: discord.Interaction):
     Arguments:
         interaction: Discord Interaction from CommandTree.
     """
-    is_success = random.random() < 0.5
+    is_lucky = random.random() < 0.5
 
-    if is_success:
-        embed = build_response_embed(
+    dice_image_url = (
+        "https://oldschool.runescape.wiki/images/thumb/Dice_(6).png/245px-Dice_(6).png"
+    )
+
+    dice_embed = discord.Embed(
+        description=f"### {interaction.user.mention} is rolling for an RNG reset...",
+        color=discord.Colour.blurple(),
+    )
+    dice_embed.set_image(url=dice_image_url)
+
+    dice_message = await interaction.followup.send(embed=dice_embed)
+
+    await asyncio.sleep(5)
+
+    await dice_message.delete()
+
+    if is_lucky:
+        result_embed = build_response_embed(
             title="RNG Reset Successful!",
             description="Your RNG has been restored. That pet is definitely dropping on your next kill. Probably.",
             color=discord.Colour.green(),
         )
-        embed.set_thumbnail(
+        result_embed.set_thumbnail(
             url="https://oldschool.runescape.wiki/images/thumb/Reward_casket_%28master%29.png/150px-Reward_casket_%28master%29.png"
         )
     else:
-        embed = build_response_embed(
+        result_embed = build_response_embed(
             title="You rolled a 0!",
             description="The RNG gods have denied your request. Enjoy staying dry for another 10,000 kills.",
             color=discord.Colour.red(),
         )
-        embed.set_thumbnail(
+        result_embed.set_thumbnail(
             url="https://oldschool.runescape.wiki/images/thumb/Skull.png/130px-Skull.png"
         )
 
-    await interaction.followup.send(embed=embed)
+    await interaction.followup.send(embed=result_embed)
