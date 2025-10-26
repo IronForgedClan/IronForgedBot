f76f38b87f358c8f181711620039002335d9636d
Barnz <hi@barnz.dev>
2025-10-26 10:30:07 +0000

    fix ephemeral decorators

--- ironforgedbot/decorators/require_role.py
+++ ironforgedbot/decorators/require_role.py
@@ -9,8 +9,13 @@ from ironforgedbot.state import STATE
 logger = logging.getLogger(__name__)
 
 
-def require_role(role: ROLE):
-    """Makes sure that the interaction user has the required role or higher"""
+def require_role(role: ROLE, ephemeral: bool = False):
+    """Makes sure that the interaction user has the required role or higher
+
+    Args:
+        role: The minimum role required to execute the command
+        ephemeral: Whether to defer the interaction ephemerally (default: False)
+    """
 
     from ironforgedbot.common.helpers import normalize_discord_string
 
@@ -56,7 +61,7 @@ def require_role(role: ROLE):
                 )
 
             if not interaction.response.is_done():
-                await interaction.response.defer(thinking=True)
+                await interaction.response.defer(ephemeral=ephemeral)
 
             return await func(*args, **kwargs)
 
