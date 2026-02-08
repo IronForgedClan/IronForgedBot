ce6ac2c8ea977c4a559f0bd91320c077afff2e4b
Barnz <hi@barnz.dev>
2026-02-08 17:55:27 +0000

    view ingots remove thumbnail and prevent line wrapping

--- alembic/versions/8797e836629f_add_member_boolean_flags.py
+++ alembic/versions/8797e836629f_add_member_boolean_flags.py
@@ -11,7 +11,6 @@ from typing import Sequence, Union
 from alembic import op
 import sqlalchemy as sa
 
-
 # revision identifiers, used by Alembic.
 revision: str = "8797e836629f"
 down_revision: Union[str, None] = "32ff5bc60920"
