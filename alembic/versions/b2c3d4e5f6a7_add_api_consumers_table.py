d224eca1b94e00ac0ed70e9072a9e422e6aad538
Barnz <hi@barnz.dev>
2026-07-12 17:55:28 +0100

    resolve migration multiple heads

--- alembic/versions/b2c3d4e5f6a7_add_api_consumers_table.py
+++ alembic/versions/b2c3d4e5f6a7_add_api_consumers_table.py
@@ -13,7 +13,7 @@ import sqlalchemy as sa
 from alembic import op
 
 revision: str = "b2c3d4e5f6a7"
-down_revision: Union[str, None] = "ff14108b4c64"
+down_revision: Union[str, None] = "8797e836629f"
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
 
